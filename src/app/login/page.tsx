46c808d27fa48bfbc100d73dfb750ee60372fb4a
ZevonikRF <rifky.fachrizain@gmail.com>
2024-12-09 23:37:02 +0700

    update 3 : ngebagusin login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,18 @@
+import styles from "@/app/interface/login/login.module.css"
+
 const Login = () => {
     return (
-        <div>Login</div>
-    );
+        <div className={styles.container}>
+            <form action="" className={styles.form}>
+                <div className={styles.titlecont}>
+                    <div className={styles.title}>Cafe ABC Dashboard</div>
+                </div>
+                <input type="text" placeholder="username"></input>
+                <input type="password" placeholder="password"></input>
+                <button>Login</button>
+            </form>
+        </div>
+    )
 }
-
 
 export default Login
